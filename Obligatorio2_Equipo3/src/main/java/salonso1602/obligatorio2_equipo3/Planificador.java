bbf0e49dd513fcd37d127c772c134d7305bd4e28
Néstor Martínez <nestor.marvi@gmail.com>
2022-06-03 16:48:53 -0300

    Agregados atributos

---
+++ Obligatorio2_Equipo3/src/main/java/salonso1602/obligatorio2_equipo3/Planificador.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package salonso1602.obligatorio2_equipo3;
+
+/**
+ *
+ * @author Néstor
+ */
+public class Planificador {
+    
+}
