1e3e3846c4a9e0f679c18c2ec9f09e376b97b457
SAlonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2022-06-03 16:00:33 -0300

    added proceso

---
+++ Obligatorio2_Equipo3/src/main/java/salonso1602/obligatorio2_equipo3/Proceso.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package salonso1602.obligatorio2_equipo3;
+
+/**
+ *
+ * @author gatom
+ */
+public class Proceso {
+    public static enum Estados{
+        Listo,
+        Bloqueado,
+        Finalizado
+    }
+    private Estados estadoActual;
+    
+    private int prioridad;
+    private int tiempoEjecucion;
+    private int tiempoEjecutado;
+    private int periodoInterrupcionES;
+    private int largoInterrupcionES;
+    private String id;
+    
+    public Proceso(String nombreProceso, int Prioridad, int tiempoDeCPU, int periodoIntES, int largoIntEs){
+        id = nombreProceso;
+        prioridad = Prioridad;
+        tiempoEjecucion = tiempoDeCPU;
+        periodoInterrupcionES = periodoIntES;
+        largoInterrupcionES = largoIntEs;
+        tiempoEjecutado = 0;
+        estadoActual = Estados.Listo;
+    }
+    
+    public Estados getEstadoActual(){
+        if(tiempoEjecutado > tiempoEjecucion){
+            this.estadoActual = Estados.Finalizado;
+            return estadoActual;
+        } else if(tiempoEjecutado % periodoInterrupcionES == 0){
+            this.estadoActual = Estados.Bloqueado;
+            return estadoActual;
+        } else{
+            this.estadoActual = Estados.Listo;
+            return estadoActual;
+        }
+    }
+    
+    public void setEstadoActual(Estados estado){
+        this.estadoActual = estado;
+    }
+    public void setPrioridad(int prioridadNueva){
+        this.prioridad = prioridadNueva;
+    }
+    public void setTiempoEjecucion(int tiempoNuevo){
+        this.tiempoEjecucion = tiempoNuevo;
+    }
+    public void setTiempoEjecutado(int tiempoNuevo){
+        this.tiempoEjecutado = tiempoNuevo;
+    }
+    public void setPeriodoES(int tiempoNuevo){
+        this.periodoInterrupcionES = tiempoNuevo;
+    }
+    public void setLargoES(int tiempoNuevo){
+        this.largoInterrupcionES = tiempoNuevo;
+    }
+    public void setID(String nombreNuevo){
+        this.id = nombreNuevo;
+    }
+}
