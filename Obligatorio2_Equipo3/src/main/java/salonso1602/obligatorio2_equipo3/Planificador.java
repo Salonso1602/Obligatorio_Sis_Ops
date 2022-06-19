b24abfbb4a2e4c8cdaf12211f71947ef9cb4fc07
SAlonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2022-06-18 23:36:36 -0300

    try8

--- Obligatorio2_Equipo3/src/main/java/salonso1602/obligatorio2_equipo3/Planificador.java
+++ Obligatorio2_Equipo3/src/main/java/salonso1602/obligatorio2_equipo3/Planificador.java
@@ -121,6 +121,7 @@ public class Planificador {
                         cpu.setProcesoEnCPU(proceso);
                         logger.append("Pase " + proceso.getID() + " a CPU" + cpu.getID() + "\n");
                         procesadoresUtilizables.remove(procesadoresUtilizables.size() - 1);
+                        bloqs.add(proceso);
                         break;
                     }
                 } else if (proceso.getEstadoActual() == Proceso.Estados.Bloqueado) {
